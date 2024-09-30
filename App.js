8e7f4284a67d09e546927d999d3e4df8ca404332
yuudachi13 <t2pupa00@students.oamk.fi>
2024-09-30 15:08:06 +0300

    randomkaljafacta

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const ULR = 'https://random-data-api.com/api/v2/beers'
+
 export default function App() {
+
+  const [brand, setBrand] = useState('')
+  const [name, setName] = useState('')
+  const [style, setStyle] = useState('')
+
+  useEffect(() => {
+    fetch(ULR)
+    .then(response => response.json())
+    .then((json) => {
+      setBrand(json.brand)
+      setName(json.name)
+      setStyle(json.style)
+    }).catch((error) =>{
+      console.log(error)
+    })
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Random beer fact </Text>
+      <Text> {brand} </Text>
+      <Text> {name} </Text>
+      <Text> {style} </Text>
     </View>
   );
 }
